9b0661049756bc2a3e9f7a6fef94851c883acb68
Chayce Knaub <chayceknaub@Chayces-MacBook-Pro.local>
2023-02-02 15:02:36 -0800

    set up

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// 1. Write code to get array of names from given array of users
+// 2. Get back only active users
+// 3. Sort users by age descending
+const users = [
+    {
+      id: 1,
+      name: "Jack",
+      isActive: true,
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "John",
+      isActive: true,
+      age: 18,
+    },
+    {
+      id: 3,
+      name: "Mike",
+      isActive: false,
+      age: 30,
+    },
+  ];
